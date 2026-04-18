9a4f4b293cf60b816480e6f65b01cdb5a17a9c32
Jeffrey Caines <jeffreycaines@users.noreply.github.com>
2026-04-18 02:17:58 -0230

    Add server side validation of multiselect form and css styling

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["UPLOAD_PATH"] = file_save_location
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.png', '.jpg', '.jpeg', '.webp', '.gif']
+app.config['UPLOAD_EXTENSIONS'] = ['.png', '.jpg', '.jpeg', '.webp', '.gif', '.PNG', '.JPG', '.JPEG', '.WEBP', '.GIF']
 
 Session(app)
 
@@ -21,56 +21,56 @@ def index():
         flash("Welcome to the site, please add a Pokemom to get started!!")
     return render_template("index.html")
 
-@app.route('/insert')
+
+@app.route('/insert', methods=['POST', 'GET'])
 def insert():
-    return render_template('insert.html')
+    if request.method == 'POST':
+        pkmn_name = request.form['name']
+        pkmn_type = request.form.getlist('type')
+        if len(pkmn_type) > 2:
+            while len(pkmn_type) > 2:
+                pkmn_type.pop()
+        if len(pkmn_type) == 1:
+            pkmn_type.append("None")
+        ability = request.form['ability']
+        generation = request.form['generation']
+        img = request.files['img']
+        filename = secure_filename(img.filename)
+        if filename != "":
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            img.save(os.path.join(app.config['UPLOAD_PATH'], filename))
 
-@app.route('/result', methods=['POST'])
-def display_result():
-    pkmn_name = request.form['name']
-    pkmn_type = request.form.getlist('type')
-    # if pkmn_type.length() > 2:
-    #     pass
-    #     # remove exess list contents
-    if not pkmn_type[1]:
-        pkmn_type.append("")
-    ability = request.form['ability']
-    generation = request.form['generation']
-    img = request.files['img']
-    filename = secure_filename(img.filename)
-    if filename != "":
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)
-        img.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-
-    if "pokemon" in session:
-        session["pokemon"][pkmn_name] = {'Primary Type':pkmn_type[0], 'Secondary Type':pkmn_type[1], 'Ability':ability, 'Generation':generation, 'img':img.filename}
+        if "pokemon" in session:
+            session["pokemon"][pkmn_name] = {'Primary Type':pkmn_type[0], 'Secondary Type':pkmn_type[1], 'Ability':ability, 'Generation':generation, 'img':img.filename}
+        else:
+            session["pokemon"] = {}
+            session["pokemon"][pkmn_name] = {'Primary Type':pkmn_type[0], 'Secondary Type':pkmn_type[1], 'Ability':ability, 'Generation':generation, 'img':img.filename}
+        
+        flash(f"Added the Pokemon {pkmn_name}!")
+        return render_template('insert.html')
     else:
-        session["pokemon"] = {}
-        session["pokemon"][pkmn_name] = {'Primary Type':pkmn_type[0], 'Secondary Type':pkmn_type[1], 'Ability':ability, 'Generation':generation, 'img':img.filename}
+        return render_template('insert.html')
 
-    flash(f"Added the Pokemon {pkmn_name}!")
-    return render_template('insert.html')
 
-@app.route("/remove")
+@app.route("/remove", methods=['POST', 'GET'])
 def remove():
-    return render_template("remove.html", pokemon=session.get("pokemon", {}))
-
-@app.route("/removed", methods=['POST'])
-def removed():
-    pokemon=session.get("pokemon", {})
-    pkmn_removed = []
-    for key in request.form:
-        pkmn_removed.append(key)
-    for name in pkmn_removed:
-        pokemon.pop(name)
-        flash(f"Removed the Pokemon {name}!")
-    return render_template("remove.html", pokemon=session.get("pokemon", {})) 
+    if request.method == 'POST':
+        pokemon=session.get("pokemon", {})
+        pkmn_removed = []
+        for key in request.form:
+            pkmn_removed.append(key)
+        for name in pkmn_removed:
+            pokemon.pop(name)
+            flash(f"Removed the Pokemon {name}!")
+        return render_template("remove.html", pokemon=session.get("pokemon", {}))
+    else:
+        return render_template("remove.html", pokemon=session.get("pokemon", {}))
+    
 
 @app.route('/display')
 def display():
-    print(session)
     return render_template('display.html', pokemon=session.get("pokemon", {}), file_location=file_save_location)
 
 
--- static/form_validation.js
+++ static/form_validation.js
@@ -1,29 +1,6 @@
-const validateName = () => {
-    const name_input = document.getElementById("name");
-    const name_span = name_input.nextElementSibling;
-    if(!name_input.value || name_input.value === ""){
-        name_span.innerText = "Name required.";
-        return false;
-    }
-    name_span.innerText = "";
-    return true;
-};
-
-const validateTypes = (event) => {
-    const type_input = document.getElementById("type").innerText;
-    const type_split = type_input.split(/\r?\n/);
-    console.log(type_split)
-
-}
-
-
 document.addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById("name").addEventListener("change", validateName);
-    // document.getElementById("type").addEventListener("change", validateTypes);
-    // document.getElementById("ability").addEventListener("change", validateAbility);
-    // document.getElementById("generation").addEventListener("change", validateGeneration);
-    const form = document.querySelector('form');
     let valid = false
+    const form = document.querySelector('form');
     form.addEventListener('submit', (event) => {    
         if (!valid){
             const formData = new FormData(form);  
